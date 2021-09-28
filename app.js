626f92dd8a7a95e33cd4a2d7a7a0b6ce643b2d8d
buamorim <brunamorim.silva@outlook.com>
2021-09-28 17:17:38 -0300

    js started

---
+++ app.js
@@ -0,0 +1,4 @@
+let emailId = document.getElementById("email-id")
+let errorMsg = document.getElementById("error-msg")
+let icon = document.getElementById("icon")
+let mailRegex = /^[a-zA-Z] [a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/
